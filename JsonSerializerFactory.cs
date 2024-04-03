c36b7522213c26d99da5bd678a1934c2e899f340
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-04-03 09:18:28 -0600

    Agregar serialización y deserialización JSON

---
+++ JsonSerializerFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializacion
+{
+    public class JsonSerializerFactory : ISerializerFactory
+    {
+        public ISerializer CreateSerializer()
+        {
+            return new JsonSerializer();
+        }
+    }
+}
